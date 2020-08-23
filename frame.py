20077e172f864e96506e1ffe95cb6a64251e250e
raghawtiwari <70049200+raghawtiwari@users.noreply.github.com>
2020-08-23 14:12:30 +0530

    Update frame.py

--- frame.py
+++ frame.py
@@ -1,6 +1,11 @@
 import cv2
-cap= cv2.VideoCapture('/home/raghaw/myfirst/public/test.mp4')
 i=0
+import os
+cap= cv2.VideoCapture('./test.mp4')
+i=0
+cwd=os.getcwd()
+os.mkdir(str(cwd) + '/frmaes1')
+os.chdir(str(cwd) + '/frmaes1')
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == False:
